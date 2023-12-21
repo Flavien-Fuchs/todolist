687464932a5814806a242631a6d20a93816e4313
AYOUBA SOUMARE <ayoubasoumare99@gmail.com>
2023-12-21 20:03:47 +0100

    création de fichier js avec mes fonctions de filtre et ajout deid et data-type aux button html pour selectionner les filtres

---
+++ js/tasks.js
@@ -0,0 +1,80 @@
+export const tasks = [
+    {
+      name: "read the book",
+      done: true,
+      deadline: Date(),
+      priority: "😊",
+      description: "Pour être libre",
+      listname: "learn",
+    },
+    {
+      name: "buy meat",
+      done: false,
+      deadline: Date(),
+      priority: "😅",
+      description: "Pour la forme",
+      listname: "food",
+    },
+    {
+        name: "buy a whey",
+        done: false,
+        deadline: Date(),
+        priority: "😊",
+        description: "Pour garder la forme",
+        listname: "food",
+
+    },
+    {
+        name: "eat a apple",
+        done: true,
+        deadline: Date(),
+        priority: "😊",
+        description: "Parceque c'est bon",
+        listname: "food",
+
+    },
+    {
+        name: "vist yucatan",
+        done: true,
+        deadline: Date(),
+        priority: "😅",
+        description: "pour l'amour de l'aventure",
+        listname: "travel",
+    },
+    {
+        name: "Learn to ride a horse",
+        done: false,
+        deadline: Date(),
+        priority: "😊",
+        description: "Pour l'expérience",
+        listname: "sport",
+
+        
+    },
+    {
+        name: "training football ",
+        done: true,
+        deadline: Date(),
+        priority: "😅",
+        description: "Pour garder la forme",
+        listname: "sport",
+    },
+    {
+        name: "run across the street",
+        done: false,
+        deadline: Date(),
+        priority: "😊",
+        description: "Pour un défi amical",
+        listname: "sport",
+    },
+    {
+        name: "travel in spain",
+        done: true,
+        deadline: Date(),
+        priority: "😅",
+        description: "C'est mon plus grand rêve",
+        listname: "travel",
+    },
+    
+  ];
+  
