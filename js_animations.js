8307a39d6bbc6e0e4699479ebaabfd75c8e8f8a3
nasbous <147812688+nasbous@users.noreply.github.com>
2024-01-03 14:22:30 +0100

    Version retropédalée fonctionnelle

---
+++ js_animations.js
@@ -0,0 +1,118 @@
+// Function to apply the drop animation
+function onDropAnimation(element) {
+    element.animate([
+        {
+            transform: "scale(1.1)",
+            opacity: 1
+        },
+        {
+            transform: "scale(1)", // Change this to maintain the original size
+            opacity: 0.5 // Adjust this value as needed
+        },
+        {
+            transform: "scale(1)", // Change this to maintain the original size
+            opacity: 1 // Adjust this value as needed
+        }
+    ], {
+        duration: 500,
+        easing: 'ease-out',
+        iterations: 1,
+        direction: 'normal',
+        fill: 'forwards'
+    });
+}
+
+function onDeleteAnimation(articleElement) {
+    const emojiElement = articleElement.querySelector('.task-emoji');
+    const taskTitle = articleElement.querySelector('.task-title');
+    const taskDate = articleElement.querySelector('span');
+    const taskComment = articleElement.querySelector('div');
+
+    const animation = [
+        {
+            opacity: 1,
+            transform: 'scale(1)',
+
+        },
+        {
+            opacity: 0,
+            transform: 'scale(1.1)',
+
+        }
+    ];
+    
+    articleElement.animate(animation, {
+        duration: 900,
+
+        easing: 'ease-out',
+        iterations: 1,
+        direction: 'normal',
+        fill: 'forwards'
+    });
+
+    emojiElement.animate([
+        {
+            transform: 'translateX(0) rotate(0)',
+            opacity: 1,
+        },
+        {
+            transform: `translateX(-${articleElement.offsetWidth}px) rotate(-360deg)`,
+            opacity: 0,
+        }
+    ], {
+        duration: 700,
+        easing: 'ease-out',
+        iterations: 1,
+        direction: 'normal',
+        fill: 'forwards'
+    });
+
+    taskTitle.animate([
+
+        {
+            opacity: 1,
+        },
+        {
+            opacity: 0,
+        }
+    ], {
+        duration: 300,
+        easing: 'ease-out',
+        iterations: 1,
+        direction: 'normal',
+        fill: 'forwards'
+    });
+
+    taskDate.animate([
+
+        {
+            opacity: 1,
+        },
+        {
+            opacity: 0,
+        }
+    ], {
+        duration: 700,
+        easing: 'ease-out',
+        iterations: 1,
+        direction: 'normal',
+        fill: 'forwards'
+    });
+
+    taskComment.animate([
+
+        {
+            opacity: 1,
+        },
+        {
+            opacity: 0,
+        }
+    ], {
+        duration: 300,
+        easing: 'ease-out',
+        iterations: 1,
+        direction: 'normal',
+        fill: 'forwards'
+    });
+}
+
--- script.js
+++ script.js
@@ -1,40 +1,220 @@
-class Task {
-
-    constructor(name, done, deadLine, priority, description) {
-      this.name = name;
-      this.done = done;
-      this.deadLine = deadLine;
-      this.priority = priority;
-      this.description = description;
-    }
-
-    displayInfo() {
-        console.log(`${this.name}, ${this.done}, ${this.priority}`);
-    }
-}
-
-function addTask(task, list){
-    list.push(task)
-}
-
-function removeTask(task, toDo, done){
-    done.push(task)
-    indexToRemove = toDo.indexOf(task);
-    toDo.splice(indexToRemove, 1);
-}
+let buttonAddTask = document.querySelector(".task-add");
+let sectionToAdd = document.querySelector("section.task-list");
 
 let toDoList = [];
 let doneList = [];
 
-const firstTask = new Task("Buy coffee", false, Date(), "😊", "I'm out of coffee, I need to buy it");
-const secondTask = new Task("Do Sports", false, Date(), "😊", "Get back in shape");
-const thirdTask = new Task("Read a book", false, Date(), "😊", "I need to read more");
+function createArticle() {
+  const taskToAdd = getInput();
+  toDoList.push(taskToAdd);
 
-addTask(firstTask, toDoList);
-addTask(secondTask, toDoList);
-removeTask(secondTask, toDoList, doneList);
-removeTask(firstTask, toDoList, doneList);
-console.log(toDoList);
-console.log(" -------------------- ");
-console.log(doneList);
-console.log(" * * * ");
+  const categorySelector = document.getElementById("add-task-list");
+  const newItemArticle = document.createElement("article");
+  newItemArticle.classList.add("task-template", categorySelector.value);
+  if (taskToAdd.priority === "😅") {
+    newItemArticle.classList.add("orange");
+  } else if (taskToAdd.priority === "😊") {
+    newItemArticle.classList.add("green");
+  } else {
+    newItemArticle.classList.add("red");
+  }
+
+  newItemArticle.addEventListener("click", function () {
+    onDeleteAnimation(newItemArticle);
+    setTimeout(function () {
+      newItemArticle.remove();
+      sendToDone(newTitle.innerText);
+    }, 500);
+  });
+
+  const newTitleDiv = document.createElement("div");
+  newTitleDiv.classList.add("task-title");
+  newItemArticle.appendChild(newTitleDiv);
+
+  const newTitle = document.createElement("h3");
+  newTitle.innerText = taskToAdd.name;
+  newTitleDiv.appendChild(newTitle);
+
+  const emojiDiv = document.createElement("div");
+  emojiDiv.classList.add("task-emoji");
+  emojiDiv.innerText = taskToAdd.priority;
+  newItemArticle.appendChild(emojiDiv);
+
+  const newDate = document.createElement("span");
+  newDate.classList.add("task-date");
+  if (taskToAdd.deadLine) {
+    newDate.innerText = taskToAdd.deadLine;
+  } else {
+    newDate.classList.add("hidden");
+  }
+  newItemArticle.appendChild(newDate);
+
+  const newPlace = document.createElement("div");
+  newPlace.classList.add("task-place");
+  newPlace.classList.add("hidden");
+  newItemArticle.appendChild(newPlace);
+
+  const newComment = document.createElement("div");
+  newComment.classList.add("task-comment");
+  if (taskToAdd.deadLine) {
+    newComment.innerText = taskToAdd.description;
+  } else {
+    newComment.classList.add("hidden");
+  }
+  newItemArticle.appendChild(newComment);
+
+  newItemArticle.addEventListener("click", function () {
+    setTimeout(function () {
+      newItemArticle.remove();
+      sendToDone(newTitle.innerText);
+    }, 500);
+  });
+
+  if (sectionToAdd.hasChildNodes) {
+    const first = sectionToAdd.children[0];
+    sectionToAdd.insertBefore(newItemArticle, first);
+    DragAndDropModule.init();
+    return;
+  }
+
+  sectionToAdd.appendChild(newItemArticle);
+}
+
+buttonAddTask.addEventListener("click", createArticle);
+DragAndDropModule.init();
+
+function sendToDone(nameOfTask) {
+  let indexToRemove = toDoList.findIndex(function (obj) {
+    return obj.name === nameOfTask;
+  });
+  if (indexToRemove !== -1) {
+    doneList.push(toDoList[indexToRemove]);
+    toDoList.splice(indexToRemove, 1);
+    console.log(toDoList);
+    console.log(doneList);
+  }
+}
+
+function getInput() {
+  let taskToAdd = {
+    name: "",
+    done: false,
+    deadLine: "",
+    priority: "",
+    description: "",
+    category: "",
+  };
+
+  let taskField = document.getElementById("add-task-name");
+  if (taskField.value === "") {
+    taskToAdd.name = "Unnamed task";
+  } else {
+    taskToAdd.name = taskField.value;
+  }
+
+  let deadlineField = document.getElementById("add-task-date");
+  if (deadlineField.value === "") {
+    taskToAdd.deadLine = "Unknown deadline";
+  } else {
+    taskToAdd.deadLine = deadlineField.value;
+  }
+
+  let describeField = document.getElementById("add-task-comment");
+  if (describeField.value === "") {
+    taskToAdd.description = "No details given";
+  } else {
+    taskToAdd.description = describeField.value;
+  }
+
+  let emojiSelector = document.getElementById("add-task-emoji");
+  console.log(emojiSelector.value);
+  switch (emojiSelector.value) {
+    case "green": {
+      taskToAdd.priority = "😊";
+      break;
+    }
+    case "orange": {
+      taskToAdd.priority = "😅";
+      break;
+    }
+    case "red": {
+      taskToAdd.priority = "🫠";
+      break;
+    }
+    default: {
+      taskToAdd.priority = "😊";
+    }
+  }
+
+  console.log(taskToAdd.priority);
+
+  let categorySelector = document.getElementById("add-task-list");
+  taskToAdd.category = categorySelector.value;
+
+  return taskToAdd;
+}
+
+const listbutton = document.querySelector(".list-task");
+listbutton.addEventListener("click", (event) => {
+  const button = event.target;
+
+  if (button.tagName === "BUTTON") {
+    /* const tasklist = button.dataset.type; */
+
+    // Vérifie si le bouton est déjà actif
+    const isActive = button.classList.contains("active");
+
+    // Si le bouton est actif, retire la classe "active" et filtre par les types restants
+    if (isActive) {
+      button.classList.remove("active");
+    } else {
+      // Si le bouton n'est pas actif, ajoute la classe "active" et filtre par les types existants
+      button.classList.add("active");
+    }
+
+    // Récupère tous les boutons actifs
+    const activeButtons = document.querySelectorAll(".list-task button.active");
+
+    // Si aucun bouton n'est actif, affiche tous les éléments
+    if (activeButtons.length === 0) {
+      const articles = document.querySelectorAll(".task-template");
+      articles.forEach((article) => {
+        article.style.display = "flex";
+      });
+    } else {
+      // Si des boutons sont actifs, filtre par les types des boutons actifs
+      const selectedTypes = Array.from(activeButtons).map(
+        (activeButton) => activeButton.dataset.type
+      );
+      filterByType(selectedTypes);
+    }
+  }
+});
+
+function filterByType(selectedTypes) {
+  const articles = document.querySelectorAll(".task-template");
+  articles.forEach((article) => {
+    const articleType = article.classList[1]; // La classe correspond au type de la tâche
+    if (selectedTypes.includes(articleType)) {
+      article.style.display = "flex";
+    } else {
+      article.style.display = "none";
+    }
+  });
+}
+
+// function showPastTasks() {
+//     // Efface le contenu actuel de la section "doToday"
+//     const doTodaySection = document.querySelector(".doToday");
+//     const isActive = pastTaskButton.classList.contains("active");
+//     if (!isActive){
+//         pastTaskButton.classList.add("active");
+//         doTodaySection.innerHTML = "";
+//     } else {
+//         pastTaskButton.classList.remove("active");
+//     }
+
+// }
+
+// const pastTaskButton = document.querySelector(".actual-page button.past-task")
+// pastTaskButton.addEventListener("click", showPastTasks)
