7a81b40a76781c976483f2fd887a284a2fae2658
Flavien Fuchs <flavien.fuchs@gmail.com>
2024-01-04 14:29:38 +0100

    changement de quelques regles css mineurs

--- pointer.js
+++
@@ -1,7 +0,0 @@
-let addTaskButton = document.querySelector(".task-add");
-let addTaskContainer = document.querySelector("#form-new-task");
-
-addTaskButton.addEventListener("click", function () {
-    addTaskButton.classList.add("hidden");
-    addTaskContainer.classList.toggle("hidden");
-})
